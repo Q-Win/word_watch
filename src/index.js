d117ea0631ee4585ad2cf46182d59081e63c6814
Quinn Krug <quinn.krug@gmail.com>
2019-02-05 09:30:53 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,25 @@ import $ from 'jquery'
 $(document).ready(() => {
   // have fun!
 })
+
+
+var request = new XMLHttpRequest();
+
+function getTopWord(){
+  request.open('GET', "https://wordwatch-api.herokuapp.com/api/v1/top_word")
+  request.onload = function() {
+    var word = JSON.parse(request.responseText)
+    var key = Object.keys(word.word)
+    var count = word.word[key]
+    debugger;
+    formatWord(key, count)
+  };
+  request.send();
+
+}
+
+function formatWord(){
+    console.log("test")
+}
+
+getTopWord(word, count)
